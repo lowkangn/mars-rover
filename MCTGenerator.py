da6a5362fef505b881fba2ecae078e6fe07e46a6
Low Kang Ngee <kangngee62@gmail.com>
2023-10-31 10:15:01 +0800

    First draft of MCTS

--- MCTGenerator.py
+++ MCTGenerator.py
@@ -4,12 +4,14 @@ import math
 from numpy import log
 
 class Node(object):
-    def __init__(self, actions, state):
+    def __init__(self, action_count, action):
         self.v = 0 # estimated value
         self.n = 0 # number of visits
-        self.actions = actions
+        self.c = 2 # exploration parameter
+
+        self.action_count = action_count
+        self.action = action
         self.children = []
-        self.state = state
         self.parent = None
 
     def is_terminal(self):
@@ -17,69 +19,76 @@ class Node(object):
 
     def best_child(self):
         best = None
-        uct = 0
+        uct = -math.inf
         for child in self.children:
             if child.n == 0:
                 return child
             else:
-                uct_ = child.v + (self.c * math.svrt(log(self.n) / child.n)) # UCT formula
+                uct_ = child.v + (self.c * math.sqrt(log(self.n) / child.n)) # UCT formula
                 if uct_ > uct:
                     uct = uct_
                     best = child
         return best
-
+    
+    def select_random(self):
+        return random.choice(self.children)
     
     def add_child(self, node):
         self.children.append(node)
         node.parent = self
 
 class MCTGenerator(object):
-    def __init__(self, env, c, gamma):
+    def __init__(self, env):
         self.env = env
-        self.disc_actions= env.disc_actions
-        self.disc_states = env.disc_states
 
         self.action_count = len(env.disc_actions)
-        self.root = Node(self.action_count, env.reset())
-        self.expand(self.root)
-        self.tm = TransitionModelGenerator().get_tm(env)
-        self.steps = env.horizon
-        self.c = c
-        self.discount = gamma
+        self.root = self.reset()
+        self.horizon = env.horizon
 
-    def select(self, node):
-        self.path = [node]
-        selected = node
-        while not selected.is_terminal():    
+        # Store the sequence of actions found by MCTS for running the simulation later.
+		# Since the environment is deterministic, the sequence of steps can be predetermined.
+        self.steps = []
+
+    def reset(self):
+        self.root = Node(self.action_count, None)
+        self.expand(self.root)
+        return self.root
+
+    def next_action(self):
+        action = max(self.root.children, key=lambda child: child.v).action
+        self.steps.append(action)
+        return action
+
+    def select(self):
+        self.env.reset()
+        for action in self.steps:
+            self.env.step(action)
+
+        selected = self.root
+        while not selected.is_terminal():
             best = selected.best_child()
-            self.path.append(best)
             selected = best
         return selected
         
     def expand(self, node):
-        initial_state = self.disc_states[node.state]
-        for a in self.disc_actions.values():
-            action, value = a.split("|")
-            new_state = self.env.state2disc(self.tm.transition(action, initial_state, value))
-            child = Node(self.action_count, new_state)
+        for a in range(self.action_count):
+            child = Node(self.action_count, a)
             node.add_child(child)
         return node.children[0]
 
-    def simulate(self, node):
-        s = self.disc_states[node.state]
+    def simulate(self):
         r = 0
-        for _ in range(0, self.steps):
-            random_a = self.disc_actions[random.randrange(self.action_count + 1)]
-            a, value = random_a.split("|")
-            s, r_ = self.tm.transition(a, s, value)
-            r += r_
-
-            if all(m == 1 for m in s[-1]): # termination condition
+        for _ in range(0, self.horizon - len(self.steps)):
+            random_a = random.randrange(self.action_count)
+            _, reward, done, _ = self.env.step(random_a)
+            r += reward
+            if done:
                 break
         return r
     
-    def update(self, node, r):
+    def update(self, node, r): # backpropagation
         node.n += 1
+        node.v += r
 
         if node.parent != None:
             self.update(node.parent, r)
--- MCTS_agent.py
+++ MCTS_agent.py
@@ -7,40 +7,58 @@ from time import time
 import tracemalloc
 
 class MCTSAgent(object):
-	def	__init__(self, env=None, gamma=0.99, c=2, max_iterations=1000):
+	def	__init__(self, env=None, gamma=0.99, max_iterations=100):
 		self.max_iterations = max_iterations
-		self.mct = MCTGenerator(env, c, gamma)
+		self.mct = MCTGenerator(env)
+		self.next_step = 0
+		self.horizon = env.horizon
 
-	def step(self, state):
-		return
+	def step(self):
+		action = self.mct.steps[self.next_step]
+		self.next_step += 1
+		return action
 
 	def solve(self):
 		"""
 		Insert solving mechanism here.
 		"""
-		return
+		for _ in range(self.horizon):
+			for _ in range(self.max_iterations):
+				selected = self.mct.select()
+
+				self.mct.expand(selected)
+
+				selected = selected.select_random()
+				self.mct.env.step(selected.action)
+
+				reward = self.mct.simulate()
+				self.mct.update(selected, reward)
+
+			self.mct.next_action()
 
 
 def main():
 	myEnv = MarsRoverDiscFactory().get_env(level='1', instance='0')
 	agent = MCTSAgent(env=myEnv)
+	agent.solve()
+	state = myEnv.reset()
 	total_reward = 0
 
-	# for step in range(myEnv.horizon):
-	# 	action = agent.step(state)
-	# 	next_state, reward, done, info = myEnv.step(action)
-	# 	total_reward += reward
-	# 	print()
-	# 	print('step       = {}'.format(step))
-	# 	print('state      = {}'.format(state), myEnv.disc_states[state])
-	# 	print('action     = {}'.format(action), myEnv.disc_actions[action])
-	# 	print('next state = {}'.format(next_state), myEnv.disc_states[next_state])
-	# 	print('reward     = {}'.format(reward))
-	# 	state = next_state
-	# 	if done:
-	# 		break
-	# print("episode ended with reward {}".format(total_reward))
-	# myEnv.close()
+	for step in range(myEnv.horizon):
+		action = agent.step()
+		next_state, reward, done, info = myEnv.step(action)
+		total_reward += reward
+		print()
+		print('step       = {}'.format(step))
+		print('state      = {}'.format(state), myEnv.disc_states[state])
+		print('action     = {}'.format(action), myEnv.disc_actions[action])
+		print('next state = {}'.format(next_state), myEnv.disc_states[next_state])
+		print('reward     = {}'.format(reward))
+		state = next_state
+		if done:
+			break
+	print("episode ended with reward {}".format(total_reward))
+	myEnv.close()
 
 if __name__ == "__main__":
 	start = time()
