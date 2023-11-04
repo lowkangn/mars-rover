4d7e2e8bf123ded31f1504dc7cb0c2145a535cfa
TijmenKuijpers <tijmen.kuijpers@gmail.com>
2023-11-04 13:12:11 +0800

    jhhk

--- MarsRoverDisc.py
+++ MarsRoverDisc.py
@@ -37,19 +37,21 @@ class MarsRoverDisc(Env):
         # set up the environment visualizer
         self.base_env.set_visualizer(MarsRoverVisualizer)
 
-        # global variables (applicable to all levels)
+        # global variables
+        self.rover_step_size = int(self.base_env.sampler.subs['MAX-STEP'][0])
+        self.x_bound = int(self.base_env.sampler.subs['MAX-X'])
+        self.y_bound = int(self.base_env.sampler.subs['MAX-Y'])
         self.mineral_count = int(len(self.base_env.sampler.subs['MINERAL-VALUE']))
         self.mineral_pos = list(zip(self.base_env.sampler.subs['MINERAL-POS-X'], self.base_env.sampler.subs['MINERAL-POS-Y']))
         self.mineral_values = self.base_env.sampler.subs['MINERAL-VALUE']
-        self.mineral_areas = self.base_env.sampler.subs['MINERAL-AREA']
 
         self.disc_states = {}
-        self.disc_actions = self.init_actions()
+        self.disc_actions = {}
         self.Prob = {}
 
     def initialize(self):
-        # do not use this for larger instances
         self.disc_states = self.init_states()
+        self.disc_actions = self.init_actions()
         self.Prob = self.generate_tm()
 
     def init_states(self):
@@ -85,12 +87,12 @@ class MarsRoverDisc(Env):
     
     def generate_tm(self):
         try:
-            with open(f'level {self.level}/instance{self.instance}.pickle', 'rb') as f:
-                tm =  pickle.load(f)
-                f.close()
-                return tm
+            f = open(f'level {self.level}/instance{self.instance}.pickle', 'rb')
+            tm =  pickle.load(f)
+            f.close()
+            return tm
         except:
-            model = TransitionModelGenerator().get_tm(self)
+            model = TransitionModelGenerator(self, level=self.level, instance=self.instance)
             return model.generate_transitions()
     
     def step(self, action):
@@ -107,11 +109,11 @@ class MarsRoverDisc(Env):
     
     def disc2action(self, a):
         '''
-        Converts discrete action into the Mars Rover environment.
+        Converts discrete action into Level 1 Mars Rover environment.
         Input:
             - a (int): action
         Return:
-            - a (definition): action that is compatible with the Mars Rover environment.
+            - a (definition): action that is compatible with Level 1 Mars Rover environment.
         '''
         a_def = self.disc_actions[a]
         a_desc, value = a_def.split('|')
@@ -120,29 +122,12 @@ class MarsRoverDisc(Env):
 
     def disc2state(self, s):
         '''
-        Converts discrete state into the Mars Rover environment state.
+        Converts discrete state into Level 1 Mars Rover environment state.
         Input:
             - s (int): action
         Return:
-            - s (definition): state that is compatible with the Mars Rover environment.
+            - s (definition): state that is compatible with Level 1 Mars Rover environment.
         '''
-        pass
-
-    def state2disc(self, state):
-        pass
-
-"""
-For levels 1 and 2, where the rover's movement is based on displacement.
-"""  
-class DispMarsRoverDisc(MarsRoverDisc):
-    def __init__(self, level='1', instance='0'): 
-        super().__init__(level, instance)
-        # level specific global variables
-        self.x_bound = int(self.base_env.sampler.subs['MAX-X'])
-        self.y_bound = int(self.base_env.sampler.subs['MAX-Y'])
-        self.rover_step_size = int(self.base_env.sampler.subs['MAX-STEP'][0])
-
-    def disc2state(self, s):
         s_def = self.disc_states[s]
         state = {}
         state['pos-x___d1'] = s_def[0][0]
@@ -166,69 +151,42 @@ class DispMarsRoverDisc(MarsRoverDisc):
 
         return None
     
-class LevelOneEnv(DispMarsRoverDisc):
+class LevelOneEnv(MarsRoverDisc):
     def __init__(self, instance='0'): 
         super().__init__('1', instance)
 
-class LevelTwoEnv(DispMarsRoverDisc):
+class LevelTwoEnv(MarsRoverDisc):
     def __init__(self, instance='0'): 
         super().__init__('2', instance)
+        # level specific global variables
+        self.mineral_areas = self.base_env.sampler.subs['MINERAL-AREA']
 
 class LevelThreeEnv(MarsRoverDisc):
-    def __init__(self, instance='0'):
+    def __init__(self, instance='0'):   
         super().__init__('3', instance)
         # level specific global variables
-        self.rover_max_power = int(self.base_env.sampler.subs['MAX-POWER'])
-        self.rover_max_vel = int(self.base_env.sampler.subs['MAX-VEL'])
-        self.x_bound = self.horizon * self.rover_max_vel # the max distance the rover can possibly travel in a single direction
-        self.y_bound = self.x_bound
+        self.mineral_areas = self.base_env.sampler.subs['MINERAL-AREA']
 
-        
     def init_states(self):
-        '''
-        Initialise discrete space
-        '''
-        # each possible position of the rover
-        rover_possible_pos = list(itertools.product(np.arange(-self.x_bound, self.x_bound + 1), np.arange(-self.y_bound, self.y_bound + 1)))
-
-        # each possible velocity of the rover
-        rover_possible_vel = list(itertools.product(np.arange(-self.rover_max_vel, self.rover_max_vel + 1), np.arange(-self.rover_max_vel, self.rover_max_vel + 1)))
-
-        # all combinations of each mineral being harvested/not harvested
-        m_harvested_combinations = list(itertools.product(np.arange(0, 2), repeat=self.mineral_count))
-
-        # each state is represented in the form ((rover_pos_x, rover_pos_y), (rover_vel_x, rover_vel_y), (m1_harvested, m2_harvested, ... ))
-        states = list(itertools.product(rover_possible_pos, rover_possible_vel, m_harvested_combinations))
-        disc_states = {}
-
-        for i, _v in enumerate(states):
-            disc_states[i] = _v
-        return bidict(disc_states)
+        super().init_states()
+    
+    def init_actions(self):
+        super().init_actions()
+    
+    def step(self, action):
+        super().step(action)
+    
+    def reset(self, seed=None):
+        super().reset()
+    
+    def render(self):
+        super().render()
+    
+    def disc2action(self, a):
+        super().disc2action(a)
 
     def disc2state(self, s):
-        s_def = self.disc_states[s]
-        state = {}
-        state['vel-x___d1'] = s_def[1][0]
-        state['pos-x___d1'] = s_def[0][0]
-        state['vel-y___d1'] = s_def[1][1]
-        state['pos-y___d1'] = s_def[0][1]  
-        for i in range(0, self.mineral_count):
-            state[f'mineral-harvested___m{i + 1}'] = s_def[2][i]
-        return state
+        super().disc2state(s)
 
     def state2disc(self, state):
-        rover_pos_x = state['pos-x___d1']
-        rover_pos_y = state['pos-y___d1']
-        rover_pos = (rover_pos_x, rover_pos_y)
-        rover_vel_x = state['vel-x___d1']
-        rover_vel_y = state['vel-y___d1']
-        rover_vel = (rover_vel_x, rover_vel_y)        
-        mineral_harvest = []
-        for i in range(1, self.mineral_count + 1):
-            mineral_harvest.append(state[f'mineral-harvested___m{i}'])
-        disc_state = (rover_pos, rover_vel, tuple(mineral_harvest))
-
-        if disc_state in self.disc_states.inverse:
-            return self.disc_states.inverse[disc_state]
-
-        return None
+        super().state2disc(state)
